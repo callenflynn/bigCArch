d52fb45e857f6b1ceba4570958e1ffa3e283cab0
callenflynn <ctflynn10@icloud.com>
2026-09-20 01:43:52 -0400

    feat: add zoxide initialization to fish config

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,9 +1,9 @@
-
 # overwrite greeting
 # potentially disabling fastfetch
 #function fish_greeting
 #    # smth smth
 #end
 starship init fish | source
+zoxide init fish | source
 function fish_greeting
 end
